0531f42ec5d94d37397c57758d57e3ae625c5865
turamusy <iwilltryhardboiz@gmail.com>
2022-01-17 11:58:09 +0300

    project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-native/no-inline-styles': 0,
+    'no-shadow': 'off',
+    'eslint-comments/no-unlimited-disable': 1,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
